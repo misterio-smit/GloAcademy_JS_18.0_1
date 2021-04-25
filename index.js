7d7f34009e147db744c15bbcb9cc265849b5b6d3
Igor <57453357+misterio-smit@users.noreply.github.com>
2021-04-26 01:07:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+const buttonStart = document.getElementById('start');
+console.log(buttonStart);
+
+const buttonPlusExpenses = document.getElementsByTagName('button')[0];
+console.log(buttonPlusExpenses);
+const bduttonPlusIncome = document.getElementsByTagName('button')[1];
+console.log(bduttonPlusIncome);
+
+const checkBox = document.querySelector('#deposit-check');
+console.log(checkBox);
+const additional = document.querySelectorAll(".additional_income-item");
+console.log(additional);
+const values = document.getElementsByClassName('result-total');
+console.log(values);
+const check = document.querySelector('input');
+console.log(check);
