33955f9b5bacbfccd6ae264c8c95112d0058ddf5
Igor <57453357+misterio-smit@users.noreply.github.com>
2021-04-29 21:49:43 +0700

    mission complit

--- index.js
+++ index.js
@@ -4,34 +4,55 @@
 const buttonStart = document.getElementById('start');
 console.log(buttonStart);
 
-const buttonPlusExpenses = document.getElementsByTagName('button')[0];
-console.log(buttonPlusExpenses);
-const bduttonPlusIncome = document.getElementsByTagName('button')[1];
-console.log(bduttonPlusIncome);
+const budgetDay = document.getElementsByClassName('result-total')[1];
+console.log(budgetDay);
 
-const checkBox = document.querySelector('#deposit-check');
-console.log(checkBox);
-const additional = document.querySelectorAll(".additional_income-item");
-console.log(additional);
+const expensesMonth = document.getElementsByClassName('result-total')[2];
+console.log(expensesMonth);
 
-const resultTotal1 = document.getElementsByClassName('result-total')[1];
-console.log(resultTotal1);
-const resultTotal2 = document.getElementsByClassName('result-total')[2];
-console.log(resultTotal2);
-const resultTotal3 = document.getElementsByClassName('result-total')[3];
-console.log(resultTotal3);
-const resultTotal4 = document.getElementsByClassName('result-total')[4];
-console.log(resultTotal4);
-const resultTotal5 = document.getElementsByClassName('result-total')[5];
-console.log(resultTotal5);
-const resultTotal6 = document.getElementsByClassName('result-total')[6];
-console.log(resultTotal6);
+const additionalIncome = document.getElementsByClassName('result-total')[3];
+console.log(additionalIncome);
 
+const additionalExpensesResult = document.getElementsByClassName('result-total')[4];
+console.log(additionalExpensesResult);
+
+const incomePeriod = document.getElementsByClassName('result-total')[5];
+console.log(incomePeriod);
+
+const targetMonth = document.getElementsByClassName('result-total')[6];
+console.log(targetMonth);
 
 const resultTotal8 = document.querySelector('.result-total');
 console.log(resultTotal8);
 
-const input = document.querySelector('input');
-console.log(input);
-const deposit = document.querySelector('.deposit');
-console.log(deposit);
+const salaryAmount = document.querySelector('.salary-amount');
+console.log(salaryAmount);
+
+const incomeAmount = document.querySelector('.income-title');
+console.log(incomeAmount);
+
+const incomeItem = document.querySelectorAll('.income-items');
+console.log(incomeItem);
+
+const buttonPlusExpenses = document.getElementsByTagName('button')[0];
+console.log(buttonPlusExpenses);
+
+const additional = document.querySelectorAll(".additional_income-item");
+console.log(additional);
+
+const expensesItems = document.querySelectorAll('.expenses-items');
+console.log(expensesItems);
+
+const bduttonPlusIncome = document.getElementsByTagName('button')[1];
+console.log(bduttonPlusIncome);
+
+const additionalExpenses = document.querySelectorAll('.additional_expenses');
+console.log(additionalExpenses);
+
+const depositCheckBox = document.querySelector('#deposit-check');
+console.log(depositCheckBox);
+
+const periodSelect = document.querySelector('.period-select');
+console.log(periodSelect);
+
+
